723eab62d064f22fcf68f22ae365ebb981358e9c
Ivan <1095036273@qq.com>
2022-01-10 21:38:28 +0800

    feat: send

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ const port = 3000
 app.get("/", (req, res) => {
   // 响应方法
   // 下载
-  //   res.download("./api/login.js")
+  res.download("./api/login.js")
   // 快速结束响应
   //   res.end()
   //   res.json(null)
   //   res.json({ user: "tobi" })
-  res.status(500).json({ error: "message" })
+  //   res.status(500).json({ error: "message" })
+  //   res.jsonp({ user: "tobi" })
+  // 默认302进行重定向
+  //   res.redirect("http://example.com")
+  //   需要配合专门模板引擎
+  //   res.render("index", function (err, html) {
+  //     res.send(html)
+  //   })
+  // res.send 发送各种消息
+  //   res.set("Content-Type", "text/html")
+  //   res.send(Buffer.from("<p>some html</p>"))
 })
 
 app.listen(port, () => {
