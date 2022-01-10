7d7461b43fdc0653a7bd3ceeddfa2d842002fc3b
Ivan <1095036273@qq.com>
2022-01-10 21:26:26 +0800

    feat: res.json

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const port = 3000
 app.get("/", (req, res) => {
   // 响应方法
   // 下载
-  res.download("./api/login.js")
+  //   res.download("./api/login.js")
+  // 快速结束响应
+  //   res.end()
+  //   res.json(null)
+  //   res.json({ user: "tobi" })
+  res.status(500).json({ error: "message" })
 })
 
 app.listen(port, () => {
