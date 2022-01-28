80dd2549ee2f1ea85b4e897a4c00550c2bf556fc
Ivan <1095036273@qq.com>
2022-01-28 11:14:32 +0800

    fix: 优化代码

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # express-demo
+
+运行
+
+```bash
+node index.js
+```
--- index.js
+++ index.js
@@ -7,30 +7,8 @@ app.get("/success", (req, res) => {
 app.get("/success-message", (req, res) => {
   res.json({ code: -100, message: "一个小小的错误" })
 })
-app.get("/end", (req, res) => {
-  res.end()
-})
 app.get("/error", (req, res) => {
-  // 响应方法
-  // 下载
-  // res.download("./api/login.js")
-  // 快速结束响应
-  //   res.end()
-  //   res.json(null)
-  //   res.json({ user: "tobi" })
   res.status(500).json({ error: "message" })
-  //   res.jsonp({ user: "tobi" })
-  // 默认302进行重定向
-  //   res.redirect("http://example.com")
-  //   需要配合专门模板引擎
-  //   res.render("index", function (err, html) {
-  //     res.send(html)
-  //   })
-  // res.send 发送各种消息
-  // res.set("Content-Type", "text/html");
-  // res.send(Buffer.from("<p>some html</p>"));
-  res.send(req.ip)
-  // res.sendStatus(404);
 })
 
 app.use(function (err, req, res, next) {
