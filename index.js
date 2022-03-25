95541319d6b5e2bdb633a058cd618f6a7a382e7f
Ivan <1095036273@qq.com>
2022-03-26 00:01:14 +0800

    修改端口

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 3001
 const host = "127.0.0.1"
 console.log(process.env)
 app.get("/success", (req, res) => {
